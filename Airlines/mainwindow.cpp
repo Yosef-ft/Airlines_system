ba9134f0c4c9ef9d00907ec62a1f5b35d3f63786
Yosef-ft <yosefetene02@gmail.com>
2024-02-17 15:51:30 +0300

    Added the database connector

--- Airlines/mainwindow.cpp
+++ Airlines/mainwindow.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QSqlDatabase>
+#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
+    bool ok = connect_db();
+    if(ok) {
+        qDebug () << "Database connected" << Qt::endl;
+    }
+    else
+        qDebug() << "Unable t connect database" << Qt::endl;
+
     ui->setupUi(this);
 }
 
--- Airlines/mainwindow.h
+++ Airlines/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -12,6 +13,29 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+public:
+    QSqlDatabase db;
+
+    void close_db(){
+        db.close();
+    }
+
+    bool connect_db (){
+        db = QSqlDatabase::addDatabase("QPSQL");
+        db.setHostName("localhost");
+        db.setDatabaseName("Airlines_DB");
+        db.setUserName("postgres");
+        db.setPassword("root");
+        bool ok = db.open();
+
+        if (ok){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
 public:
     MainWindow(QWidget *parent = nullptr);
